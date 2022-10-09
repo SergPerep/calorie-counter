e50727e03f103a1c46fe351c6375db70bd5f18fd
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-10-09 19:12:42 +0200

    Feat: add arrows to the nav to click through dates

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,10 +1,32 @@
+import Icon from "./BaseUI/Icon";
 import CalDropDown from "./CalDropdown/CalDropdown";
+import date from "date-and-time";
+import { useNavigate } from "react-router-dom";
 
 const Nav = ({ selectedDateStr }: { selectedDateStr: string }) => {
+  const navigate = useNavigate();
+
+  const moveDate = (dayOffsetNum: number) => {
+    const selectedDate = new Date(selectedDateStr);
+    const updatedSelectedDate = date.addDays(selectedDate, dayOffsetNum);
+    const updatedSelectedDateStr = date.format(
+      updatedSelectedDate,
+      "YYYY-MM-DD"
+    );
+    navigate(`/${updatedSelectedDateStr}`);
+  };
   return (
     <nav>
       <div className="container">
         <CalDropDown selectedDateStr={selectedDateStr} />
+        <div className="arrow-buttons">
+          <button onClick={() => moveDate(-1)}>
+            <Icon name="chevron_left" />
+          </button>
+          <button onClick={() => moveDate(1)}>
+            <Icon name="chevron_right" />
+          </button>
+        </div>
       </div>
     </nav>
   );
