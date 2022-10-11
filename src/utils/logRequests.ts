8012517188905ddda481d0ea2cd0b66c099e42f7
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-10-11 21:38:01 +0200

    Build: remove server

--- src/utils/logRequests.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import log from "./log";
-// Middleware that logs request into terminal
-const logRequests = (req: Request, res: Response, next: NextFunction) => {
-  log.green(`-- ${req.method} ${req.path}`);
-  next();
-};
-
-export default logRequests;
