ee37548056f6ad0b0a39a41340fbf4e34b7acbba
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-10-17 10:27:44 +0200

    Feat: return server back

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import logRequests from "./utils/logRequests";
+import recordsAPI from "./components/records/recordsAPI";
+import handleErrors from "./components/errors/handleErrors";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(logRequests);
+app.use(express.json());
+
+app.use("/records", recordsAPI);
+
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Calorie Counter API</h1>");
+  res.end();
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello");
+});
+
+app.use(handleErrors);
+
+export default app;
