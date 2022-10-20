45af836c71e8e3c8830645d174775bf8d7fa60e5
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-10-20 13:55:44 +0200

    Test: remove testForStatusCode, add checkBasics and genMissingFieldCases

--- src/components/records/records.test.ts
+++ src/components/records/records.test.ts
@@ -1,7 +1,21 @@
-import request from "supertest";
+import request, { Response } from "supertest";
 import app from "../../app";
 import connectToDB, { mongod } from "../../db";
-import { faker } from "@faker-js/faker";
+import sc from "../../utils/statusCodes";
+interface ExpectedOptions {
+  statusCode: number;
+  status?: "success" | "fail" | "error";
+  message?: string;
+  errorMessage?: string;
+}
+
+const checkBasics = (response: Response, expectedOptions: ExpectedOptions) => {
+  const { statusCode, status, message, errorMessage } = expectedOptions;
+  expect(response.statusCode).toBe(statusCode);
+  if (status) expect(response.body.status).toBe(status);
+  if (message) expect(response.body.message).toBe(message);
+  if (errorMessage) expect(response.body.error).toBe(errorMessage);
+};
 
 beforeAll(async () => {
   // Connect to DB
@@ -28,19 +42,58 @@ const testForStatusCode = (
 const getRandomItem = (items: unknown[]) =>
   items[Math.floor(Math.random() * items.length)];
 
+const genMissingFieldCases = (body: { [key: string]: any }) => {
+  const fieldNames = Object.keys(body);
+  return fieldNames.map((fieldName) => {
+    const updatedBody = { ...body };
+    delete updatedBody[fieldName];
+    return {
+      body: updatedBody,
+      missingField: fieldName,
+    };
+  });
+};
+
 // GET RECORDS FOR A DATE
 describe("GET /", () => {
   const method = "get";
   const uri = "/records?date=2022-10-10";
-  testForStatusCode(uri, method, 200);
+  it("OK", async () => {
+    const response = await request(app).get(uri);
+    checkBasics(response, { statusCode: sc.OK });
+  });
 });
 
 // CREATE A RECORD
 describe("POST /", () => {
   const uri = "/records";
+  const body = {
+    date: "2022-10-10",
+    meal_type: "breakfast",
+    ingredient: "Coocoos",
+    fats_per_100: 0.2,
+    carbs_per_100: 4.03,
+    proteins_per_100: 0.3,
+    quantity: 120,
+    unit: "ml",
+  };
+
+  const missingFieldCases = genMissingFieldCases(body);
 
   it("OK", async () => {
-    const body = {};
     const response = await request(app).post(uri).send(body);
+    expect(response.statusCode).toBe(sc.CREATED);
+    checkBasics(response, { statusCode: sc.CREATED });
+  });
+
+  it("Missing field", () => {
+    missingFieldCases.forEach(async (testCase) => {
+      const response = await request(app).post(uri).send(testCase.body);
+      checkBasics(response, {
+        statusCode: sc.BAD_REQUEST,
+        status: "error",
+        errorMessage: `Empty field: ${testCase.missingField}`,
+      });
+    });
   });
 });
